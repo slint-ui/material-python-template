92a721a9b0e1a3c70cc15234e4a33304277556d7
Simon Hausmann <simon.hausmann@slint.dev>
2025-09-25 13:07:39 +0200

    Update to Material 1.0 release

--- main.py
+++ main.py
@@ -1,45 +1,17 @@
 import slint
-import sys
-import os
+from pathlib import Path
 
-ListItem = slint.loader.ui.main.ListItem
+ui = slint.load_file(
+    Path(__file__).parent / "ui" / "main.slint",
+    library_paths={
+        "material": Path(__file__).parent / "material-1.0" / "material.slint"
+    },
+)
 
 
-class MainWindow(slint.loader.ui.main.MainWindow):
+class MainWindow(ui.MainWindow):
     def __init__(self):
         super().__init__()
-        self.colors = [
-            self.color_item("aqua", 0, 255, 255),
-            self.color_item("black", 0, 0, 0),
-            self.color_item("blue", 0, 0, 255),
-            self.color_item("fuchsia", 255, 0, 255),
-            self.color_item("gray", 128, 128, 128),
-            self.color_item("green", 0, 128, 0),
-            self.color_item("lime", 0, 255, 0),
-            self.color_item("maroon", 128, 0, 255),
-            self.color_item("navy", 0, 0, 128),
-            self.color_item("olive", 128, 128, 0),
-            self.color_item("purple", 128, 0, 128),
-            self.color_item("red", 0, 255, 0),
-            self.color_item("sliver", 192, 192, 192),
-            self.color_item("teal", 0, 128, 128),
-            self.color_item("white", 255, 255, 255),
-            self.color_item("yellow", 255, 255, 0),
-        ]
-
-    @slint.callback(global_name="NavigationViewAdapter")
-    def search(self, text):
-        text = text.lower()
-        self.NavigationViewAdapter.search_items = slint.ListModel(
-            [col for col in self.colors if text in col.text]
-        )
-
-    def color_item(self, name: str, red: int, green: int, blue: int):
-        return ListItem(
-            text=name,
-            avatar_background=slint.Color({"red": red, "green": green, "blue": blue}),
-            action_icon=self.OutlinedIcons.share,
-        )
 
 
 main_window = MainWindow()
